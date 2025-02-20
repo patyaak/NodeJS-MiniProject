e97c2044e0b32759ea949112376edd24843ab68d
Patyaak <pmulguthi12@gmail.com>
2025-02-20 16:56:59 +0545

    login added

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const userModel = require("./models/user");
+const postModel = require("./models/post");
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -9,7 +12,51 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.render("index", {});
+  res.render("index");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/register", async (req, res) => {
+  let { email, password, username, name, age } = req.body;
+
+  let user = await userModel.findOne({ email });
+  if (user) {
+    return res.status(500).send("Email already exists");
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let user = await userModel.create({
+        username,
+        name,
+        email,
+        password: hash,
+        age,
+      });
+
+      let token = jwt.sign({ email: email, userid: user._id }, "shhhh");
+      res.cookie("token", token);
+      res.send("registered");
+    });
+  });
+});
+
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+  if (!user) {
+    return res.status(500).send("Something went wrong");
+  }
+
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (result) {
+      res.status(200).send("logged in");
+    } else {
+      res.redirect("/login");
+    }
+  });
 });
 
 app.listen(5000);
--- models/user.js
+++ models/user.js
@@ -7,6 +7,12 @@ const userSchema = mongoose.Schema({
   password: String,
   email: String,
   age: Number,
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+  ],
 });
 
 module.exports = mongoose.model("user", userSchema);
