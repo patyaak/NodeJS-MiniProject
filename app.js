df8f853d161b098bc2ed674eb4e51c207e161e67
Patyaak <pmulguthi12@gmail.com>
2025-02-21 18:52:39 +0545

    profile added

--- app.js
+++ app.js
@@ -19,9 +19,16 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
+//if user is logged in only then we can view the profile
+app.get("/profile", isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  res.render("profile", { user });
+});
+
 app.post("/register", async (req, res) => {
   let { email, password, username, name, age } = req.body;
 
+  //checking if the email is already used or not
   let user = await userModel.findOne({ email });
   if (user) {
     return res.status(500).send("Email already exists");
@@ -37,6 +44,7 @@ app.post("/register", async (req, res) => {
         age,
       });
 
+      //after registration setting up the cookies
       let token = jwt.sign({ email: email, userid: user._id }, "shhhh");
       res.cookie("token", token);
       res.send("registered");
@@ -46,17 +54,43 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   let { email, password } = req.body;
+
+  let user = await userModel.findOne({ email });
   if (!user) {
     return res.status(500).send("Something went wrong");
   }
 
   bcrypt.compare(password, user.password, (err, result) => {
     if (result) {
-      res.status(200).send("logged in");
+      let token = jwt.sign({ email: email, userid: user._id }, "shhhh");
+      res.cookie("token", token);
+      res.status(200).redirect("/profile");
     } else {
       res.redirect("/login");
     }
   });
 });
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.redirect("login");
+});
+
+function isLoggedIn(req, res, next) {
+  //if token is empty then the user is not logged in
+  if (req.cookies.token == "") {
+    res.send(`
+      <script>
+        alert('You must be logged in');
+        window.location.href = '/login';
+      </script>
+    `);
+  } else {
+    //the user is logged in cookie is cleared
+    let data = jwt.verify(req.cookies.token, "shhhh");
+    req.user = data;
+    next();
+  }
+}
 
 app.listen(5000);
